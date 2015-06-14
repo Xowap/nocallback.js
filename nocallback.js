ac9730840f8bb2be98b8f91c9406eb0b437db3f4
Rémy Sanchez <remy.sanchez@hyperthese.net>
2015-06-14 12:39:21 +0200

    Coroutines return promises

--- nocallback.js
+++ nocallback.js
@@ -1,20 +1,22 @@
-(function ($) {
+(function (exports) {
     'use strict';
 
     var exceptionHandler,
-        coroutine;
+        coroutine,
+        sleep;
 
     coroutine = function (fn) {
         function run(err, it) {
             var tick;
 
-            tick = function (prev) {
+            tick = function (prev, resolve) {
                 var cur;
 
                 try {
                     cur = it.next(prev);
 
                     if (cur.done) {
+                        resolve(cur.value);
                         return;
                     }
 
@@ -23,13 +25,13 @@
                             tick({
                                 success: true,
                                 args: arguments
-                            });
+                            }, resolve);
                         },
                         function () {
                             tick({
                                 success: false,
                                 args: arguments
-                            })
+                            }, resolve);
                         }
                     );
                 } catch (e) {
@@ -46,9 +48,9 @@
                 }
             };
 
-            (function () {
-                tick();
-            }());
+            return new Promise(function (resolve) {
+                tick(undefined, resolve);
+            });
         }
 
         return function () {
@@ -56,29 +58,32 @@
         }
     };
 
+    sleep = function (timeMsec) {
+        return new Promise(function (resolve) {
+            setTimeout(resolve, timeMsec);
+        });
+    };
+
+    exports.coroutine = coroutine;
+    exports.sleep = sleep;
+
     function test() {
         var doShit = coroutine(function* doShit() {
-            var result = yield $.get('data.json');
+                var result = yield $.get('data.json');
 
-            if (result.success) {
-                console.log('success', result);
-            } else {
-                console.log('fail');
-            }
+                if (result.success) {
+                    return result.args[0];
+                }
+            }),
+            doMoreShit = coroutine(function* doMoreShit() {
+                var result = yield doShit();
 
-            result = yield $.get('pouet.json');
+                if (result.success) {
+                    console.log('done shit', result.args[0]);
+                }
+            });
 
-            if (result.success) {
-                console.log('success', result);
-            } else {
-                console.log('fail', result);
-            }
-
-            throw new Error('woops');
-        });
-
-        doShit();
-        doShit();
+        doMoreShit();
     }
 
     test();
@@ -86,4 +91,4 @@
     exceptionHandler = function (x) {
         console.log(x.stack);
     }
-}(jQuery));
+}({}));
